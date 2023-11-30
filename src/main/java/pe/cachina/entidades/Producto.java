78d5023bcc28a54e8e3d30d9e57c2fa02a7d24a5
Alejandro <causa2234@gmail.com>
2023-11-29 20:11:57 -0500

    correcciones Lombok

--- src/main/java/pe/cachina/entidades/Producto.java
+++ src/main/java/pe/cachina/entidades/Producto.java
@@ -8,12 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+
 
 @Entity
-@Getter
-@Setter
+@Data
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
