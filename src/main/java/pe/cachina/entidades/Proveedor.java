886d35804add02f999164c6e1ac7f9c5bb16f219
Alejandro <causa2234@gmail.com>
2023-11-29 20:13:37 -0500

    validación de campo vacio

--- src/main/java/pe/cachina/entidades/Proveedor.java
+++ src/main/java/pe/cachina/entidades/Proveedor.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,7 @@ public class Proveedor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     @Column(name = "nombre_prove")
     private String nombre;
     @Column(name = "descripcion_prove")
